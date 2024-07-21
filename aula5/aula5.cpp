7a00cc4aa85c9d8506d5fec2a84172a8545af773
Thomas <thomas2003.mayer@gmail.com>
2024-07-21 20:38:52 -0300

    contantes

---
+++ aula5/aula5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define nome std::cout << "Thomas Mayer Garcez" << std::endl;
+
+int main() {
+
+    int vidas = 3, tiros = 50, life = 20 ;
+
+    std::cout << pi << std::endl;
+    nome
+
+    return 0;
+}
