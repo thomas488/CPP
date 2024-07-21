5ebe1d24907a5590e0f4f4f895443b963b50db06
Thomas <thomas2003.mayer@gmail.com>
2024-07-21 20:24:59 -0300

    variaveis e seu tipos

--- Test/hello.cpp
+++ Test/hello.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 
-int main(){
+using namespace std;
 
-    int n1 = 20;
-    
-    std::cout << n1;
+int main() {
+
+    int vida = 0; //1 2 3
+    char letras = 'S'; //B
+    //char letras[20]; //vetor com até 20 caracteres
+    double decimal = 5.5; //2.4999
+    float decimal2 = 6.6; //2.5
+    bool vivo = true; // true = verdadeiro (1) false = falso (0)
+    string nome = "Thomas"; //textos
+
+    std::cout << "Escreve o numero de vidas: " << std::endl;
+    std::cin >> vida;
+    std::cout << "Digite uma letra: " << std::endl;
+    std::cin >> letras;
+    std::cout << "Digite uma número: " << std::endl;
+    std::cin >> decimal;
+    std::cout << "Digite seu nome: : " << std::endl;
+    std::cin >> nome;
+
+    std::cout << vida << "\n" << letras << "\n" << decimal << "\n" << decimal2 << "\n" << vivo << "\n" << nome << std::endl;
 
     return 0;
 }
